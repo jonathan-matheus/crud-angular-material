c620e14a980f76ea58eca2e83f49fa498bbc99b8
jonathan-matheus <johhn.dev@gmail.com>
2025-09-02 19:44:45 -0300

    iniciando o formulário de clientes

---
+++ src/app/consulta/consulta.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-consulta',
+  imports: [],
+  templateUrl: './consulta.html',
+  styleUrl: './consulta.scss',
+})
+export class Consulta {}
