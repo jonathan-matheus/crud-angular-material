20165f9d4dc6ed903a5eb8dc92b4f232b9e6021f
jonathan-matheus <johhn.dev@gmail.com>
2025-09-02 22:59:00 -0300

    finalizando a parte visual do formulário

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, RouterLink, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, RouterLink, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -4,10 +4,20 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
