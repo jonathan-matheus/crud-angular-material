f51d3dec1fd2593ac71c5b3366d19cd78b49b475
jonathan-matheus <johhn.dev@gmail.com>
2025-09-05 17:18:39 -0300

    criando o repositorio de clientes para persistir os dados

--- src/app/cliente-service.ts
+++ src/app/cliente-service.ts
@@ -7,7 +7,21 @@ import { Clientes } from './cadastro/clientes';
 export class ClienteService {
   constructor() {}
 
+  static REPO_CLIENTES = '_CLIENTES';
+
   salvar(cliente: Clientes) {
     console.log(cliente);
+  }
+
+  obterStorage(): Clientes[] {
+    const respositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (respositorioClientes) {
+      const clientes: Clientes[] = JSON.parse(respositorioClientes);
+      return clientes;
+    }
+
+    const clientes: Clientes[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 }
